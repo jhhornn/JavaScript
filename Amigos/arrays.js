9f85225fa7fb85850ca3784d52687201663ff0d8
DebbDev <82146998+Dhebby-Opeyemi@users.noreply.github.com>
2022-08-12 12:44:47 +0100

    Added some comment to the comparison.js,Added multidimensional array to the array.js,and a bit of additional variable to the arithemetic ope

--- Amigos/arrays.js
+++ Amigos/arrays.js
@@ -32,3 +32,15 @@ console.log(`${Anna} ${Mariam} ${Joe}`);
 console.log(`${a} ${b}`);
 console.log(theRest);
 console.log(theRest.length);
+
+// Multiple Dimensional Array with indexes
+const debbyArray = [[1,2,3], 
+                    [4,5,6], 
+                    [7,8,9] 
+                    [[10,11,12], [13,14,15],16,17]
+                    [18, 19, 20],
+                ];
+
+debbyArray[3][0][1]; //[13,14,15]
+debbyArray[4]; // [18,19,20]
+debbyArray[3][0]; //[10,11,12]
